af6a6060a8b8177ff3185e994a4836ccda2a5b4c
Sajib Chakma <chakma.sajib95@yahoo.com>
2020-11-22 20:10:58 +0800

    fix: updated linkkart module

--- functions/linkkart.js
+++ functions/linkkart.js
@@ -1,6 +1,7 @@
 const getLinks = require("./utils/getLinks");
 const createLink = require("./utils/createLink");
 const updateLink = require("./utils/updateLink");
+const deleteLink = require("./utils/deleteLink");
 const formattedResponse = require("./utils/formatetedResponse");
 
 exports.handler = async event => {
@@ -10,6 +11,8 @@ exports.handler = async event => {
     return await createLink(event);
   } else if (event.httpMethod === "PUT") {
     return await updateLink(event);
+  } else if (event.httpMethod === "DELETE") {
+    return await deleteLink(event);
   }
-  return formattedResponse(200, "Hello World");
+  return formattedResponse(405, "");
 };
